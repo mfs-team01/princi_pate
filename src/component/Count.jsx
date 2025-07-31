d3d316198592139f6fa6a5000eadf98059779bd3
PRINCI-PATEL <principatel6307@gmail.com>
2025-07-31 15:48:36 +0530

    hi

--- src/component/Count.jsx
+++ src/component/Count.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import "./Count.css"
+import React, { useState } from "react";
+import "./Count.css";
 const Count = () => {
+    const [count, setCount] = useState(0);
     return (
         <div className="counter_container">
-            <button id="btn">-</button>
-              <button id="btn"></button>
-            <button id="btn">+</button>
-
-          
-
-
-
-
+            <p id="para">You have clicked {count} times</p>
+            <div className="counter-buttons">
+                <button id="btn" onClick={() => setCount(count - 1)}>-</button>
+                <button id="btn">{count}</button>
+                <button id="btn" onClick={() => setCount(count + 1)}>+</button>
+            </div>
+            <div>
+                <button id="rbtn" onClick={() => setCount(0)}>Reset</button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Count
+export default Count;
